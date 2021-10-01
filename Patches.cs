163bba7ba450fef4868a1861811ae95dce66285a
Nicholas Dunnaway <nick@dunnaway.org>
2021-10-01 12:07:21 -0700

    Fixed a few bugs around Server Startup Added Player Join/Leave messages.

--- Patches.cs
+++ Patches.cs
@@ -5,7 +5,7 @@ namespace DiscordConnector.Patches
     internal class ZNetPatches
     {
 
-        [HarmonyPatch(typeof(ZNet), "LoadWorld")]
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.LoadWorld))]
         internal class LoadWorld
         {
             private static void Postfix(ref ZNet __instance)
@@ -19,7 +19,7 @@ namespace DiscordConnector.Patches
             }
         }
 
-        [HarmonyPatch(typeof(ZNet), "Shutdown")]
+        [HarmonyPatch(typeof(ZNet),  nameof(ZNet.Shutdown))]
         internal class Shutdown
         {
             private static void Prefix(ref ZNet __instance)
@@ -31,6 +31,42 @@ namespace DiscordConnector.Patches
                         );
                 }
             }
+        }
+
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_CharacterID))]
+        internal class RPC_CharacterID
+        {
+          private static void Postfix(ZRpc rpc, ZDOID characterID)
+          {
+            if (Plugin.StaticConfig.PlayerJoinMessageEnabled)
+            {
+              ZNetPeer peer = ZNet.instance.GetPeer(rpc);
+              if (peer != null)
+              {
+                DiscordApi.SendMessage(
+                  $"{peer.m_playerName} {Plugin.StaticConfig.JoinMessage}"
+                );
+              }
+            }
+          }
+        }
+
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_Disconnect))]
+        internal class RPC_Disconnect
+        {
+          private static void Prefix(ZRpc rpc)
+          {
+            if (Plugin.StaticConfig.PlayerLeaveMessageEnabled)
+            {
+              ZNetPeer peer = ZNet.instance.GetPeer(rpc);
+              if (peer != null)
+              {
+                DiscordApi.SendMessage(
+                  $"{peer.m_playerName} {Plugin.StaticConfig.LeaveMessage}"
+                );
+              }
+            }
+          }
         }
     }
 }
--- PluginConfig.cs
+++ PluginConfig.cs
@@ -19,13 +19,17 @@ namespace DiscordConnector
         private ConfigEntry<bool> serverLaunchToggle;
         private ConfigEntry<bool> serverLoadedToggle;
         private ConfigEntry<bool> serverStopToggle;
+        private ConfigEntry<bool> playerJoinToggle;
+        private ConfigEntry<bool> playerLeaveToggle;
 
         // Logged Information Messages
         private ConfigEntry<string> serverLaunchMessage;
         private ConfigEntry<string> ServerLoadedMessage;
         private ConfigEntry<string> ServerStopMessage;
+        private ConfigEntry<string> PlayerJoinMessage;
+        private ConfigEntry<string> PlayerLeaveMessage;
 
-        public PluginConfig(ConfigFile config)
+    public PluginConfig(ConfigFile config)
         {
             PluginConfig.config = config;
             LoadConfig();
@@ -59,6 +63,18 @@ namespace DiscordConnector
                 "If enabled, this will send a message to Discord when the server shuts down." + Environment.NewLine +
                 "EX: Server has started. | Server has stopped.");
 
+            playerJoinToggle = config.Bind<bool>(NOTIFICATION_SETTINGS,
+              "Player Join Notifications",
+              true,
+              "If enabled, this will send a message to Discord when a player joins the server." + Environment.NewLine +
+              "EX: Player has joined");
+
+            playerLeaveToggle = config.Bind<bool>(NOTIFICATION_SETTINGS,
+              "Player Leave Notifications",
+              true,
+              "If enabled, this will send a message to Discord when a player leaves the server." + Environment.NewLine +
+              "EX: Player has left.");
+
             // Message Settings
 
             serverLaunchMessage = config.Bind<string>(NOTIFICATION_CONTENT_SETTINGS,
@@ -76,18 +92,32 @@ namespace DiscordConnector
                 "Server is stopping.",
                 "Set the message that will be sent when the server shuts down.");
 
+            PlayerJoinMessage = config.Bind<string>(NOTIFICATION_CONTENT_SETTINGS,
+              "Player Join Message",
+              "has joined.",
+              "Set the message that will be sent when a player joins the server");
+
+            PlayerLeaveMessage = config.Bind<string>(NOTIFICATION_CONTENT_SETTINGS,
+              "Player Leave Message",
+              "has left.",
+              "Set the message that will be sent when a player leaves the server.");
+
 
             config.Save();
         }
 
         // Exposed Config Values
 
-        public string WebHookURL { get => webhookUrl.Value; }
-        public string LaunchMessage { get => serverLaunchMessage.Value; }
-        public string LoadedMessage { get => ServerLoadedMessage.Value; }
-        public string StopMessage { get => ServerStopMessage.Value; }
-        public bool LaunchMessageEnabled { get => serverLaunchToggle.Value; }
-        public bool LoadedMessageEnabled { get => serverLaunchToggle.Value; }
-        public bool StopMessageEnabled { get => serverLaunchToggle.Value; }
+        public string WebHookURL => webhookUrl.Value;
+        public string LaunchMessage => serverLaunchMessage.Value;
+        public string LoadedMessage => ServerLoadedMessage.Value;
+        public string StopMessage => ServerStopMessage.Value;
+        public string JoinMessage => PlayerJoinMessage.Value;
+        public string LeaveMessage => PlayerLeaveMessage.Value;
+        public bool LaunchMessageEnabled => serverLaunchToggle.Value;
+        public bool LoadedMessageEnabled => serverLoadedToggle.Value;
+        public bool StopMessageEnabled => serverStopToggle.Value;
+        public bool PlayerJoinMessageEnabled => playerJoinToggle.Value;
+        public bool PlayerLeaveMessageEnabled => playerLeaveToggle.Value;
     }
 }
