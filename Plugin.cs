42fbf9ff71be721a34df0253213af2a0ce3060f6
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-06 10:49:13 -0400

    ⌛ send leaderboard messages on timer

--- Plugin.cs
+++ Plugin.cs
@@ -16,6 +16,8 @@ namespace DiscordConnector
         {
             StaticLogger = base.Logger;
             StaticConfig = new PluginConfig(base.Config);
+            StaticRecords = new Records(BepInEx.Paths.GameRootPath);
+
             var harmony = new Harmony(PluginInfo.PLUGIN_ID);
 
             // Plugin startup logic
@@ -31,9 +33,32 @@ namespace DiscordConnector
                 return;
             }
 
-            StaticRecords = new Records(BepInEx.Paths.GameRootPath);
+            if (StaticConfig.StatsAnnouncementEnabled)
+            {
+                System.Timers.Timer leaderboardTimer = new System.Timers.Timer();
+                leaderboardTimer.Elapsed += new System.Timers.ElapsedEventHandler(SendLeaderboardAnnouncement);
+                // Interval is learned from config file in seconds
+                leaderboardTimer.Interval = 1000 * StaticConfig.StatsAnnouncementPeriod;
+                leaderboardTimer.Start();
+            }
 
             harmony.PatchAll();
+        }
+
+        private void SendLeaderboardAnnouncement(object sender, System.Timers.ElapsedEventArgs elapsedEventArgs)
+        {
+            var deathLeader = StaticRecords.Retrieve(Categories.Death);
+            var joinLeader = StaticRecords.Retrieve(Categories.Join);
+            var shoutLeader = StaticRecords.Retrieve(Categories.Shout);
+            var pingLeader = StaticRecords.Retrieve(Categories.Ping);
+
+            List<Tuple<string, string>> leaderFields = new List<Tuple<string, string>>();
+            if (deathLeader.Item2 > 0) { leaderFields.Add(Tuple.Create("Most Deaths", $"{deathLeader.Item1} ({deathLeader.Item2})")); }
+            if (joinLeader.Item2 > 0) { leaderFields.Add(Tuple.Create("Most Joins|Leaves", $"{joinLeader.Item1} ({joinLeader.Item2})")); }
+            if (shoutLeader.Item2 > 0) { leaderFields.Add(Tuple.Create("Most Shouts", $"{shoutLeader.Item1} ({shoutLeader.Item2})")); }
+            if (pingLeader.Item2 > 0) { leaderFields.Add(Tuple.Create("Most Pings", $"{pingLeader.Item1} ({pingLeader.Item2})")); }
+
+            DiscordApi.SendMessageWithFields("Current leaderboard for tracked stats:", leaderFields);
         }
     }
 }
