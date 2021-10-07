510f446917fbea26386a5b6c955ae857f347fb37
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-07 14:32:12 -0400

    🧼 remove extraneous comment lines

--- src/Config/TogglesConfig.cs
+++ src/Config/TogglesConfig.cs
@@ -1,36 +1,3 @@
-// toggles
-
-// toggles.messages
-// - launch 
-// - loaded
-// - stop
-// - join
-// - leave
-// - death
-// - shout
-// - ping
-
-// toggles.position
-// - join
-// - leave
-// - pings
-// - shouts
-// - deaths
-
-// toggles.stats
-// - allow record join
-// - allow record ..
-// - allow record ..
-// - allow record ..
-// - allow record ..
-
-// toggles.leaderboard
-// - send pings leaderboard
-// - send join leaderboard
-// - send .. leaderboard
-// - send .. leaderboard
-// - send .. leaderboard
-
 using System;
 using BepInEx.Configuration;
 
