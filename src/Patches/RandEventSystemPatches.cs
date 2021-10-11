b570b42d065c2cee4bb18fe7068087575407a25b
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-11 10:09:07 -0400

    🎺 working event announcements

--- src/Patches/RandEventSystemPatches.cs
+++ src/Patches/RandEventSystemPatches.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using UnityEngine;
 
 namespace DiscordConnector.Patches
@@ -10,12 +11,12 @@ namespace DiscordConnector.Patches
         internal class SetRandomEvent
         {
 
-            private static void Postfix(ref RandomEvent ev, ref Vector3 pos)
+            private static void Prefix(ref RandomEvent ev, ref Vector3 pos)
             {
                 if (ev == null)
                 {
                     Plugin.StaticLogger.LogDebug(
-                        $"Random event cleared at {pos}"
+                        $"Random event cleared"
                     );
                     return;
                 }
@@ -24,25 +25,66 @@ namespace DiscordConnector.Patches
                 string name = ev.m_name;
                 string message = ev.m_startMessage;
                 Plugin.StaticLogger.LogDebug(
-                    $"Random event setRE? {name}: {active} for {duration} at {pos}. \"{message}\""
+                    $"Random event system SetRandomEvent? {name}: {active} for {duration} at {pos}. \"{message}\""
                 );
             }
         }
+
+    }
+    internal class RandEventPatches
+    {
 
         [HarmonyPatch(typeof(RandomEvent), nameof(RandomEvent.OnActivate))]
         internal class OnActivate
         {
 
-            private static void Postfix(ref RandomEvent __instance)
+            private static void Prefix(ref RandomEvent __instance)
             {
                 bool active = __instance.m_active;
                 float duration = __instance.m_duration;
                 string name = __instance.m_name;
                 string message = __instance.m_startMessage;
+                float time = __instance.m_time;
+                float remaining = duration - time;
                 Vector3 pos = __instance.m_pos;
                 Plugin.StaticLogger.LogDebug(
-                    $"Random event onStart? {name}: {active} for {duration} at {pos}. \"{message}\""
+                    $"Random event OnActivate {name}: {active} for {duration} at {pos}. \"{message}\" (time: {time})"
                 );
+
+                DiscordApi.SendMessage(
+                    $"{name} event is active at {pos} for {(Math.Round((decimal)remaining, 0))} more seconds!"
+                );
+            }
+        }
+
+        [HarmonyPatch(typeof(RandomEvent), nameof(RandomEvent.OnDeactivate))]
+        internal class OnDeactivate
+        {
+
+            private static void Prefix(ref RandomEvent __instance)
+            {
+                bool active = __instance.m_active;
+                float duration = __instance.m_duration;
+                string name = __instance.m_name;
+                string message = __instance.m_startMessage;
+                float time = __instance.m_time;
+                Vector3 pos = __instance.m_pos;
+                Plugin.StaticLogger.LogDebug(
+                    $"Random event OnDeactivate {name}: {active} for {duration} at {pos}. \"{message}\" (time: {time})"
+                );
+
+                if (time < duration)
+                {
+                    DiscordApi.SendMessage(
+                        $"{name} event paused at {pos} because no active players are in the area."
+                    );
+                }
+                else
+                {
+                    DiscordApi.SendMessage(
+                        $"{name} event has completed!"
+                    );
+                }
             }
         }
     }
