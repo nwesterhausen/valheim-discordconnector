1d963cfad7b3cf65f346a74c4aefb2b7cfc56420
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-13 16:36:22 -0400

    🎲 add random string generation utility

--- src/Utility.cs
+++ src/Utility.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
-using System.Text;
-using System.Text.Json;
-using UnityEngine;
+using System.Security.Cryptography;
 
 namespace DiscordConnector
 {
-    static class IpifyAPI
+    internal static class IpifyAPI
     {
         private const string ENDPOINT = "https://api64.ipify.org";
+
+        /// <summary>
+        /// Get your public IP address (either IPv4 or IPv6, preferring IPv4) according to https://api64.ipify.org
+        /// </summary>
+        /// <returns>Your public IP address</returns>
         public static string PublicIpAddress()
         {
             string ipAddress = "";
@@ -39,5 +42,83 @@ namespace DiscordConnector
             return ipAddress;
         }
 
+    }
+
+    internal static class Utiltity
+    {
+        /// <summary>
+        /// Generate a random string <paramref name="length"/> characters long. Characters will be chosen randomly from
+        /// the list "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
+        /// </summary>
+        /// <param name="length">Desired length of the random string.</param>
+        /// <returns>A random alphanumeric string.</returns>
+        public static string RandomAlphanumericString(int length)
+        {
+            const string allowed_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+            var random = new CryptoRandom();
+            string randomString = new string(Enumerable.Repeat(allowed_chars, length)
+                                                    .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return randomString;
+        }
+        /// <summary>
+        /// A More "Random" Random from .NET Matters 10/02/2019. https://docs.microsoft.com/en-us/archive/msdn-magazine/2007/september/net-matters-tales-from-the-cryptorandom
+        /// An implementation of System.Random using RNGCryptoServiceProvider.
+        /// </summary>
+        public class CryptoRandom : System.Random
+        {
+            private RNGCryptoServiceProvider _rng =
+                new RNGCryptoServiceProvider();
+            private byte[] _uint32Buffer = new byte[4];
+
+            public CryptoRandom() { }
+            public CryptoRandom(Int32 ignoredSeed) { }
+
+            public override Int32 Next()
+            {
+                _rng.GetBytes(_uint32Buffer);
+                return BitConverter.ToInt32(_uint32Buffer, 0) & 0x7FFFFFFF;
+            }
+
+            public override Int32 Next(Int32 maxValue)
+            {
+                if (maxValue < 0)
+                    throw new ArgumentOutOfRangeException("maxValue");
+                return Next(0, maxValue);
+            }
+
+            public override Int32 Next(Int32 minValue, Int32 maxValue)
+            {
+                if (minValue > maxValue)
+                    throw new ArgumentOutOfRangeException("minValue");
+                if (minValue == maxValue) return minValue;
+                Int64 diff = maxValue - minValue;
+                while (true)
+                {
+                    _rng.GetBytes(_uint32Buffer);
+                    UInt32 rand = BitConverter.ToUInt32(_uint32Buffer, 0);
+
+                    Int64 max = (1 + (Int64)UInt32.MaxValue);
+                    Int64 remainder = max % diff;
+                    if (rand < max - remainder)
+                    {
+                        return (Int32)(minValue + (rand % diff));
+                    }
+                }
+            }
+
+            public override double NextDouble()
+            {
+                _rng.GetBytes(_uint32Buffer);
+                UInt32 rand = BitConverter.ToUInt32(_uint32Buffer, 0);
+                return rand / (1.0 + UInt32.MaxValue);
+            }
+
+            public override void NextBytes(byte[] buffer)
+            {
+                if (buffer == null) throw new ArgumentNullException("buffer");
+                _rng.GetBytes(buffer);
+            }
+        }
     }
 }
