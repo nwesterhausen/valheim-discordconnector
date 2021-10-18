e70d3f333dd6e887126dc087580f17285bf3918a
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-18 17:36:20 -0400

    🚚 put game patches in own class file

--- src/Patches/ZNetPatches.cs
+++ src/Patches/ZNetPatches.cs
@@ -3,48 +3,6 @@ using HarmonyLib;
 
 namespace DiscordConnector.Patches
 {
-    internal class GamePatches
-    {
-
-        [HarmonyPatch(typeof(Game), nameof(Game.Awake))]
-        internal class LoadWorld
-        {
-            private static void Prefix()
-            {
-                if (Plugin.StaticConfig.LaunchMessageEnabled)
-                {
-                    DiscordApi.SendMessage(
-                        Plugin.StaticConfig.LaunchMessage.Replace("%PUBLICIP%", Plugin.PublicIpAddress)
-                    );
-                }
-            }
-        }
-
-
-        [HarmonyPatch(typeof(Game), nameof(Game.OnApplicationQuit))]
-        internal class Shutdown
-        {
-            [HarmonyBefore(new string[] { "HackShardGaming.WorldofValheimServerSideCharacters" })]
-            private static void Prefix()
-            {
-                if (Plugin.StaticConfig.StopMessageEnabled)
-                {
-                    DiscordApi.SendMessage(
-                        Plugin.StaticConfig.StopMessage.Replace("%PUBLICIP%", Plugin.PublicIpAddress)
-                        );
-                }
-            }
-            private static void Postfix()
-            {
-                if (Plugin.StaticConfig.ShutdownMessageEnabled)
-                {
-                    DiscordApi.SendMessage(
-                        Plugin.StaticConfig.ShutdownMessage.Replace("%PUBLICIP%", Plugin.PublicIpAddress)
-                        );
-                }
-            }
-        }
-    }
     internal class ZNetPatches
     {
 
