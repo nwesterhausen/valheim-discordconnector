b1a249ae1f10ce0263e75e8772b983ab2b19ac34
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-10-29 09:01:33 -0400

    attempted hook 1

---
+++ src/Patches/EnvManPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace DiscordConnector.Patches
+{
+    internal class EnvManPatches
+    {
+
+        [HarmonyPatch(typeof(EnvMan), nameof(EnvMan.OnMorning))]
+        internal class OnMorning
+        {
+            private static void Postfix(EnvMan __instance)
+            {
+                Plugin.StaticLogger.LogDebug("I think it's morning.");
+                if (Plugin.StaticConfig.AnnounceNewDay)
+                {
+                    string message = MessageTransformer.FormatNewDayMessage(__instance.GetCurrentDay());
+                    DiscordApi.SendMessage(message);
+                }
+            }
+        }
+    }
+}
