1f0cde39540965004a07120303772b78e4490b50
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-05-18 14:34:15 -0400

    fix: :bug: handle ipifiy.org request errors
    On failures to reach ipify.org to get the public IP for the server, an ulgy error message would be sent to the console. With this try catch, on an error to retrieve the public IP a "blank" IP is provided instead (`::0`).
    
    #107

--- src/Utility.cs
+++ src/Utility.cs
@@ -21,25 +21,35 @@ namespace DiscordConnector
             WebRequest request = WebRequest.Create(ENDPOINT);
             request.Method = "GET";
 
-            WebResponse response = request.GetResponse();
-            Plugin.StaticLogger.LogDebug($"Response Short Code (ipify.org): {((HttpWebResponse)response).StatusDescription}");
-
-            // Get the stream containing content returned by the server.
-            // The using block ensures the stream is automatically closed.
-            using (Stream dataStream = response.GetResponseStream())
+            // Wrap firing the response in a TRY/CATCH and on an exception, abandon trying to retreive the public IP
+            try
             {
-                // Open the stream using a StreamReader for easy access.
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
-                ipAddress = reader.ReadToEnd();
-                // Display the content.
-                Plugin.StaticLogger.LogDebug($"Full response (ipify): {ipAddress}");
+                WebResponse response = request.GetResponse();
+                Plugin.StaticLogger.LogDebug($"Response Short Code (ipify.org): {((HttpWebResponse)response).StatusDescription}");
+
+                // Get the stream containing content returned by the server.
+                // The using block ensures the stream is automatically closed.
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    ipAddress = reader.ReadToEnd();
+                    // Display the content.
+                    Plugin.StaticLogger.LogDebug($"Full response (ipify): {ipAddress}");
+                }
+
+                // Close the response.
+                response.Close();
+
+                return ipAddress;
             }
-
-            // Close the response.
-            response.Close();
-
-            return ipAddress;
+            catch (Exception e)
+            {
+                Plugin.StaticLogger.LogWarning("Failure to get public IP from ipify.org");
+                Plugin.StaticLogger.LogWarning(e);
+            }
+            return "::0";
         }
 
     }
