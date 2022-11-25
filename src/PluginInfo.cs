63d582290e20852937dc5b1c008c42b4d144f53b
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-11-25 12:18:41 -0500

    chore: 🔖 bump release 2.0.7

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -17,7 +17,7 @@ namespace DiscordConnector
     {
         public const string PLUGIN_ID = "games.nwest.valheim.discordconnector";
         public const string PLUGIN_NAME = "Valheim Discord Connector";
-        public const string PLUGIN_VERSION = "2.0.6";
+        public const string PLUGIN_VERSION = "2.0.7";
         public const string PLUGIN_REPO_SHORT = "github: nwesterhausen/valheim-discordconnector";
         public const string PLUGIN_AUTHOR = "Nicholas Westerhausen";
     }
