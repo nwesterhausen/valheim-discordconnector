cd7ef566af4ba4e542572c0f4b85fdd7be07e322
Nick Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2024-06-13 12:35:18 -0400

    wip: add more configuration to the model

--- src/Config/Model.cs
+++ src/Config/Model.cs
@@ -49,6 +49,14 @@ internal class Model
     /// Configuration for Discord integration
     /// </summary>
     public DiscordConfig Discord { get; set; } = new DiscordConfig();
+    /// <summary>
+    /// Configuration for messages that can be sent to Discord
+    /// </summary>
+    public Messages Messages { get; set; } = new Messages();
+    /// <summary>
+    /// Debug options
+    /// </summary>
+    public DebugOptions Debug { get; set; } = new DebugOptions();
   }
   /// <summary>
   /// Configuration for Discord integration
@@ -85,20 +93,18 @@ internal class Model
   /// </summary>
   public class DiscordDefaults
   {
-#nullable enable
     /// <summary>
     /// Default username to use for webhooks
     /// </summary>
-    public string? Username { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
     /// <summary>
     /// Default avatar URL to use for webhooks
     /// </summary>
-    public string? Avatar { get; set; } = string.Empty;
+    public string Avatar { get; set; } = string.Empty;
     /// <summary>
     /// Default mentions configuration to use for messages sent to Discord
     /// </summary>
-    public MentionsConfig? Mentions { get; set; } = new MentionsConfig();
-#nullable restore
+    public MentionsConfig Mentions { get; set; } = new MentionsConfig();
   }
   /// <summary>
   /// Webhook endpoint configuration. Used to provide a reference to a webhook URL.
@@ -127,20 +133,18 @@ internal class Model
     /// Which events to send to the webhook
     /// </summary>
     public List<Webhook.Event> Events { get; set; } = [Webhook.Event.ALL];
-#nullable enable
     /// <summary>
     /// A username to use for this webhook. If not provided, the default username for the webhook endpoint will be used.
     /// </summary>
-    public string? Username { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
     /// <summary>
     /// An avatar URL to use for this webhook. If not provided, the default avatar for the webhook endpoint will be used.
     /// </summary>
-    public string? Avatar { get; set; } = string.Empty;
+    public string Avatar { get; set; } = string.Empty;
     /// <summary>
     /// A mentions configuration to use for this webhook. If not provided, the default mentions configuration for the webhook endpoint will be used.
     /// </summary>
-    public MentionsConfig? Mentions { get; set; }
-#nullable restore
+    public MentionsConfig Mentions { get; set; } = new MentionsConfig();
   }
   /// <summary>
   /// What kinds of mentions are allowed in messages sent to Discord.
@@ -168,5 +172,226 @@ internal class Model
     /// </summary>
     public List<string> AllowedUsers { get; set; } = [];
   }
-
+  /// <summary>
+  /// Message configuration details. Includes a list of possible messages to send and whether to send the message.
+  /// </summary>
+  public class MessageConfig
+  {
+    /// <summary>
+    /// Whether to send this message to Discord
+    /// </summary>
+    public bool Enabled { get; set; } = true;
+    /// <summary>
+    /// Whether to include the player's position in the message. In a ping, this will be the pinged position.
+    /// </summary>
+    public bool IncludePosition { get; set; } = false;
+    /// <summary>
+    /// List of possible messages to send. One will be chosen at random.
+    /// </summary>
+    public List<string> Messages { get; set; } = [];
+    /// <summary>
+    /// Whether to send a special additional message for the first time this event occurs.
+    /// </summary>
+    public bool FirstTimeMessageEnabled { get; set; } = false;
+    /// <summary>
+    /// List of possible messages to send for the first time this event occurs. One will be chosen at random.
+    /// </summary>
+    public List<string> FirstTimeMessages { get; set; } = [];
+  }
+  /// <summary>
+  /// Configuration for messages that can be sent to Discord
+  /// </summary>
+  public class Messages
+  {
+    /// <summary>
+    /// Message configuration for a server launch event, when the server is starting up.
+    /// </summary>
+    public MessageConfig ServerLaunch { get; set; } = new()
+    {
+      Messages = ["Server is starting up!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for a server start event, when the server has started (and is ready to accept connections).
+    /// </summary>
+    public MessageConfig ServerStart { get; set; } = new()
+    {
+      Messages = ["Server has started!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for a server stop event, when the server is stopping.
+    /// </summary>
+    public MessageConfig ServerStop { get; set; } = new()
+    {
+      Messages = ["Server is stopping!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for a server shutdown event, when the server has stopped.
+    /// </summary>
+    public MessageConfig ServerShutdown { get; set; } = new()
+    {
+      Messages = ["Server has stopped!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for a server save event, when the server is saving the world.
+    /// </summary>
+    public MessageConfig ServerSave { get; set; } = new()
+    {
+      Messages = ["The world has been saved!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for a new day event, when a new day begins.
+    /// </summary>
+    public MessageConfig NewDayMessage { get; set; } = new()
+    {
+      Messages = ["Day %DAY_NUMBER% Begins!"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for when a player joins the server.
+    /// </summary>
+    public MessageConfig PlayerJoin { get; set; } = new()
+    {
+      Messages = ["%PLAYER_NAME% has joined the server! Online now: %NUM_PLAYERS%"],
+      Enabled = true,
+      FirstTimeMessages = ["%PLAYER_NAME% has joined the server for the first time!"],
+      FirstTimeMessageEnabled = true
+    };
+    /// <summary>
+    /// Message configuration for when a player leaves the server.
+    /// </summary>
+    public MessageConfig PlayerLeave { get; set; } = new()
+    {
+      Messages = ["%PLAYER_NAME% has left the server! Online now: %NUM_PLAYERS%"],
+      Enabled = true,
+      FirstTimeMessages = ["%PLAYER_NAME% has left the server for the first time!"],
+      FirstTimeMessageEnabled = false
+    };
+    /// <summary>
+    /// Message configuration for when a player dies.
+    /// </summary>
+    public MessageConfig PlayerDeath { get; set; } = new()
+    {
+      Messages = ["%PLAYER_NAME% has died!"],
+      Enabled = true,
+      FirstTimeMessages = ["%PLAYER_NAME% has died for the first time!"],
+      FirstTimeMessageEnabled = true,
+      IncludePosition = true
+    };
+    /// <summary>
+    /// Message configuration for when a player shouts.
+    /// </summary>
+    public MessageConfig PlayerShout { get; set; } = new()
+    {
+      Messages = ["%PLAYER_NAME% shouts %SHOUT%"],
+      Enabled = true,
+      FirstTimeMessages = ["%PLAYER_NAME% shouts for the first time!"],
+      FirstTimeMessageEnabled = false
+    };
+    /// <summary>
+    /// Message configuration for when a player pings the map.
+    /// </summary>
+    public MessageConfig PlayerPing { get; set; } = new()
+    {
+      Messages = ["%PLAYER_NAME% has pinged the map."],
+      Enabled = true,
+      FirstTimeMessages = ["%PLAYER_NAME% has pinged the map for the first time."],
+      FirstTimeMessageEnabled = true,
+      IncludePosition = true
+    };
+    /// <summary>
+    /// Message configuration for when an event starts.
+    /// </summary>
+    public MessageConfig EventStart { get; set; } = new()
+    {
+      Messages = ["**Event**: %EVENT_MSG%"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for when an event stops.
+    /// </summary>
+    public MessageConfig EventStop { get; set; } = new()
+    {
+      Messages = ["**Event**: %EVENT_MSG%"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for when an event is paused.
+    /// </summary>
+    public MessageConfig EventPaused { get; set; } = new()
+    {
+      Messages = ["**Event**: %EVENT_END_MSG% — for now! (Currently paused due to no players in the event area.)"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Message configuration for when an event is resumed.
+    /// </summary>
+    public MessageConfig EventResumed { get; set; } = new()
+    {
+      Messages = ["**Event**: %EVENT_START_MSG%"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Leaderboard title configuration for the top players.
+    /// </summary>
+    public MessageConfig LeaderboardTopPlayers { get; set; } = new()
+    {
+      Messages = ["Top %N% Player Leaderboards:"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Leaderboard title configuration for the bottom players.
+    /// </summary>
+    public MessageConfig LeaderboardBottomPlayers { get; set; } = new()
+    {
+      Messages = ["Bottom %N% Faction Leaderboards:"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Leaderboard title configuration for the top factions.
+    /// </summary>
+    public MessageConfig LeaderboardHighestPlayer { get; set; } = new()
+    {
+      Messages = ["Top Performer"],
+      Enabled = true
+    };
+    /// <summary>
+    /// Leaderboard title configuration for the bottom factions.
+    /// </summary>
+    public MessageConfig LeaderboardLowestPlayer { get; set; } = new()
+    {
+      Messages = ["Bottom Performer"],
+      Enabled = true
+    };
+  }
+  /// <summary>
+  /// Configuration for debug options. These enable extra tracing/logging for debugging purposes. Do not enable these unless you are debugging the plugin.
+  /// </summary>
+  public class DebugOptions
+  {
+    /// <summary>
+    /// Whether to log player position checks to the INFO channel
+    /// </summary>
+    public bool PlayerPositionChecks { get; set; } = false;
+    /// <summary>
+    /// Whether to log every event check to the INFO channel
+    /// </summary>
+    public bool EventChecks { get; set; } = false;
+    /// <summary>
+    /// Whether to log every event change to the INFO channel
+    /// </summary>
+    public bool EventChanges { get; set; } = false;
+    /// <summary>
+    /// Whether to enable logging for every database method call
+    /// </summary>
+    public bool DatabaseMethods { get; set; } = false;
+    /// <summary>
+    /// Whether to log every HTTP response to the INFO channel
+    /// </summary>
+    public bool HttpRequestResponses { get; set; } = false;
+  }
 }
