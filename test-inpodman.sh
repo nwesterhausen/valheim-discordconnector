969eccae647bdc39b78ae34514c3c3483fbab5e4
Nick Westerhausen <nwesterhausen@gmail.com>
2025-09-21 12:11:56 -0400

    include script to test

---
+++ test-inpodman.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+set -e
+
+dotnet build -c Release
+podman compose stop
+rm server-data/bepinex/plugins/DiscordConnector.dll
+cp DiscordConnector/bin/Release/net481/DiscordConnector.dll server-data/bepinex/plugins/DiscordConnector.dll
+podman compose up -d
+podman compose logs -f
